f300ddf605c75f78c7cf0442a7652206f8f1cbd3
gmeidanipour <gmeidanipour@pi86.informatik.uni-siegen.de>
2018-11-09 13:18:10 +0100

    Hello World update

--- DFA-to-TextGenerator/src/application/DFAToTextGeneratorApplication.java
+++ DFA-to-TextGenerator/src/application/DFAToTextGeneratorApplication.java
@@ -5,10 +5,12 @@ import org.eclipse.equinox.app.IApplicationContext;
 
 public class DFAToTextGeneratorApplication implements IApplication{
 
+	
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("DFAToTextGeneratorApplication.start()");
+		System.out.println("Hello World!");
 		return null;
 	}
 

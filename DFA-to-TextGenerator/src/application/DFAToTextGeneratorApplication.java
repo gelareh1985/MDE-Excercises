40e67228aedf4471b955018b316fd47706e8315e
gmeidanipour <gmeidanipour@pi86.informatik.uni-siegen.de>
2018-11-09 16:40:10 +0100

    ModelToText_First Commit

--- DFA-to-TextGenerator/src/application/DFAToTextGeneratorApplication.java
+++ DFA-to-TextGenerator/src/application/DFAToTextGeneratorApplication.java
@@ -1,19 +1,45 @@
 package application;
 
+import java.util.Iterator;
+import java.util.Vector;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 
+import exercises.ExercisesPackage;
+import exercises.State;
+import exercises.Transition;
+
 public class DFAToTextGeneratorApplication implements IApplication{
 
+	private static Vector<State> states=new Vector<State>();
+	private static Vector<Transition> transitions = new Vector<Transition>();
+	private String pathName="/home/pi/gmeidanipour/git/MDE-Excercises/Exercises/dynamicInstances/DFA.xmi";
 	
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("DFAToTextGeneratorApplication.start()");
-		System.out.println("Hello World!");
+		Resource modelResource = new ResourceSetImpl().getResource(URI.createFileURI(pathName), true);
+		
+		for (Iterator<EObject> iterator = modelResource.getAllContents(); iterator.hasNext();) {
+			EObject element = iterator.next();
+			System.out.println(element.eClass());
+			System.out.println(element.eClass().getName());
+			System.out.println(element.eClass().getEAllAttributes());
+			
+//			if (element instanceof State)
+			if (element.eClass() == ExercisesPackage.eINSTANCE.getState()) {
+				System.out.println(element);
+//				((State) element).getId()
+				System.out.println("id=" + element.eGet(ExercisesPackage.eINSTANCE.getState_Id()));
+			}
+		}
+
 		return null;
-		
-		
 	}
 
 	@Override

dfe356e82f81126cb4336e72d0c5a8616f6ff4e4
gmeidanipour <gmeidanipour@pi86.informatik.uni-siegen.de>
2018-11-09 13:08:34 +0100

    Second Commit

---
+++ DFA-to-TextGenerator/src/application/DFAToTextGeneratorApplication.java
@@ -0,0 +1,21 @@
+package application;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class DFAToTextGeneratorApplication implements IApplication{
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("DFAToTextGeneratorApplication.start()");
+		return null;
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		System.out.println("DFAToTextGeneratorApplication.stop()");
+	}
+
+}

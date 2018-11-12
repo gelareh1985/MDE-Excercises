20ff908b351e3b25e3bd8a7ce5faf883ca8535e1
gmeidanipour <gmeidanipour@pi86.informatik.uni-siegen.de>
2018-11-12 16:25:46 +0100

    DFA to Text Generator (Revised Version)

--- DFA-to-TextGenerator/src/application/DFAToTextGeneratorApplication.java
+++ DFA-to-TextGenerator/src/application/DFAToTextGeneratorApplication.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.Vector;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -16,30 +17,73 @@ import exercises.Transition;
 
 public class DFAToTextGeneratorApplication implements IApplication{
 
-	private static Vector<State> states=new Vector<State>();
-	private static Vector<Transition> transitions = new Vector<Transition>();
-	private String pathName="/home/pi/gmeidanipour/git/MDE-Excercises/Exercises/dynamicInstances/DFA.xmi";
-	
+//	private static Vector<State> states=new Vector<State>();
+//	private static Vector<Transition> transitions = new Vector<Transition>();
+	private String pathName1="/home/pi/gmeidanipour/git/MDE-Excercises/Exercises/dynamicInstances/DFA.xmi";
+	private String pathName2="/home/pi/gmeidanipour/git/MDE-Excercises/Exercises/dynamicInstances/exampleUML.uml";
+	private Resource modelResource1,modelResource2;
+
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		// TODO Auto-generated method stub
-		Resource modelResource = new ResourceSetImpl().getResource(URI.createFileURI(pathName), true);
-		
-		for (Iterator<EObject> iterator = modelResource.getAllContents(); iterator.hasNext();) {
+		modelResource1 = new ResourceSetImpl().getResource(URI.createFileURI(pathName1), true);
+		modelResource2 = new ResourceSetImpl().getResource(URI.createFileURI(pathName2), true);
+
+		System.out.println("**************** DFA OutPut (EMF Approach) ****************");
+		for (Iterator<EObject> iterator = modelResource1.getAllContents(); iterator.hasNext();) {
+
 			EObject element = iterator.next();
-			System.out.println(element.eClass());
-			System.out.println(element.eClass().getName());
-			System.out.println(element.eClass().getEAllAttributes());
-			
-//			if (element instanceof State)
-			if (element.eClass() == ExercisesPackage.eINSTANCE.getState()) {
-				System.out.println(element);
-//				((State) element).getId()
-				System.out.println("id=" + element.eGet(ExercisesPackage.eINSTANCE.getState_Id()));
-			}
+			//			System.out.println(element.eClass());
+			//			System.out.println(element.eClass().getName());
+			//			System.out.println(element.eClass().getEAllAttributes());
+			//			System.out.println(element.eGet(ExercisesPackage.eINSTANCE.getState_IsStart())+" = True");
+
+			dfaSpecificTextGenerator(element);
 		}
 
+		System.out.println("**************** DFA OutPut (Generic Approach) ****************");
+		for (Iterator<EObject> iterator = modelResource2.getAllContents(); iterator.hasNext();) {
+
+			EObject element = iterator.next();
+
+			genericEcoreTextGenerator(element);
+		}
+
+		//		stop();
+
 		return null;
+	}
+
+	private void genericEcoreTextGenerator(EObject element) {
+
+		System.out.println(" : " + element.eClass().getName());
+
+		for (EAttribute eat : element.eClass().getEAllAttributes()) {
+			System.out.println(eat.getName() + " = " + "\"" + element.eGet(eat) + "\"");
+		}
+
+	}
+
+	private void dfaSpecificTextGenerator(EObject element) {
+
+		//		if (element instanceof State)
+		if (element.eClass() == ExercisesPackage.eINSTANCE.getState()) {
+
+			System.out.println(" : " + element.eClass().getName());
+			//				((State) element).getId()
+
+			// Better:
+			System.out.println(ExercisesPackage.eINSTANCE.getState_Id().getName() + " = "+"\""+element.eGet(ExercisesPackage.eINSTANCE.getState_Id())+"\"");
+			System.out.println( ExercisesPackage.eINSTANCE.getState_IsStart().getName()+" = "+"\""+element.eGet(ExercisesPackage.eINSTANCE.getState_IsStart())+"\"");
+			System.out.println( ExercisesPackage.eINSTANCE.getState_IsEnd().getName()+" = "+"\""+element.eGet(ExercisesPackage.eINSTANCE.getState_IsEnd())+"\""+"\n");
+
+		} else if (element.eClass() == ExercisesPackage.eINSTANCE.getTransition()) {
+
+			System.out.println(" : " + element.eClass().getName());
+
+			// Better:
+			System.out.println(ExercisesPackage.eINSTANCE.getTransition_Input().getName() + " = "+"\""+element.eGet(ExercisesPackage.eINSTANCE.getTransition_Input())+"\""+"\n");
+		}
 	}
 
 	@Override
